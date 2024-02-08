a5b55c248715c96d4d5207717bc2942a10b4b99d
Seth Michael Larson <seth@python.org>
2024-02-08 16:10:19 -0600

    Add pip to SBOM at release stage

--- tests/test_sbom.py
+++ tests/test_sbom.py
@@ -1,3 +1,6 @@
+import json
+import unittest.mock
+
 import pytest
 import random
 import hashlib
@@ -58,3 +61,57 @@ def test_normalization():
         "a": [1, 2, 3, {"b": ["c", 4, ["2", 7, True, {}]]}],
         "b": [["a", 1, 2], ["b", 1, 2]]
     }
+
+
+def test_fetch_project_metadata_from_pypi(mocker):
+
+    mock_urlopen = mocker.patch("sbom.urlopen")
+    mock_urlopen.return_value = unittest.mock.Mock()
+
+    # This is only a partial response using the information
+    # that this function uses.
+    mock_urlopen.return_value.read.return_value = json.dumps({
+        "urls": [
+            {
+                "digests": {
+                    "blake2b_256": "94596638090c25e9bc4ce0c42817b5a234e183872a1129735a9330c472cc2056",
+                    "md5": "1331aabb4d1a2677f493effeebda3605",
+                    "sha256": "ea9bd1a847e8c5774a5777bb398c19e80bcd4e2aa16a4b301b718fe6f593aba2"
+                },
+                "filename": "pip-24.0.tar.gz",
+                "packagetype": "sdist",
+                "url": "https://files.pythonhosted.org/packages/.../pip-24.0.tar.gz",
+            },
+            {
+                "digests": {
+                    "blake2b_256": "8a6a19e9fe04fca059ccf770861c7d5721ab4c2aebc539889e97c7977528a53b",
+                    "md5": "74e3c5e4082113b1239ca0e9abfd1e82",
+                    "sha256": "ba0d021a166865d2265246961bec0152ff124de910c5cc39f1156ce3fa7c69dc"
+                },
+                "filename": "pip-24.0-py3-none-any.whl",
+                "packagetype": "bdist_wheel",
+                "url": "https://files.pythonhosted.org/packages/.../pip-24.0-py3-none-any.whl",
+            }
+        ]
+    }).encode()
+
+    # Default filename is the wheel
+    download_url, checksum_sha256 = sbom.fetch_package_metadata_from_pypi(
+        project="pip",
+        version="24.0",
+    )
+
+    mock_urlopen.assert_called_once_with("https://pypi.org/pypi/pip/24.0/json")
+    assert download_url == "https://files.pythonhosted.org/packages/.../pip-24.0-py3-none-any.whl"
+    assert checksum_sha256 == "ba0d021a166865d2265246961bec0152ff124de910c5cc39f1156ce3fa7c69dc"
+
+    # If we ask for the sdist (which we don't do normally)
+    # then it'll be returned instead.
+    download_url, checksum_sha256 = sbom.fetch_package_metadata_from_pypi(
+        project="pip",
+        version="24.0",
+        filename="pip-24.0.tar.gz"
+    )
+
+    assert download_url == "https://files.pythonhosted.org/packages/.../pip-24.0.tar.gz"
+    assert checksum_sha256 == "ea9bd1a847e8c5774a5777bb398c19e80bcd4e2aa16a4b301b718fe6f593aba2"
