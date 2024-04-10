e94518000c63bb47c04e6d49ff894e9adf0d068a
Seth Michael Larson <seth@python.org>
2024-04-10 10:16:55 -0500

    Add SBOMs generation for Windows artifacts

--- tests/test_sbom.py
+++ tests/test_sbom.py
@@ -1,6 +1,8 @@
+import pathlib
 import json
 import random
 import hashlib
+import re
 import unittest.mock
 
 import pytest
@@ -65,7 +67,6 @@ def test_normalization():
 
 
 def test_fetch_project_metadata_from_pypi(mocker):
-
     mock_urlopen = mocker.patch("sbom.urlopen")
     mock_urlopen.return_value = unittest.mock.Mock()
 
@@ -116,3 +117,81 @@ def test_fetch_project_metadata_from_pypi(mocker):
 
     assert download_url == "https://files.pythonhosted.org/packages/.../pip-24.0.tar.gz"
     assert checksum_sha256 == "ea9bd1a847e8c5774a5777bb398c19e80bcd4e2aa16a4b301b718fe6f593aba2"
+
+
+def test_create_cpython_sbom():
+    sbom_data = {"packages": []}
+
+    artifact_path = str(pathlib.Path(__file__).parent / "fake-artifact.txt")
+    sbom.create_cpython_sbom(
+        sbom_data, cpython_version="3.13.0", artifact_path=artifact_path
+    )
+
+    assert re.fullmatch(
+        r"^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z$",
+        sbom_data["creationInfo"].pop("created")
+    )
+    assert re.fullmatch(
+        r"^Tool: ReleaseTools-[a-f0-9]+$",
+        sbom_data["creationInfo"]["creators"].pop(1)
+    )
+
+    assert sbom_data == {
+        "packages": [
+            {
+                "SPDXID": "SPDXRef-PACKAGE-cpython",
+                "name": "CPython",
+                "versionInfo": "3.13.0",
+                "licenseConcluded": "PSF-2.0",
+                "originator": "Organization: Python Software Foundation",
+                "supplier": "Organization: Python Software Foundation",
+                "packageFileName": "fake-artifact.txt",
+                "externalRefs": [
+                    {
+                        "referenceCategory": "SECURITY",
+                        "referenceLocator": "cpe:2.3:a:python:python:3.13.0:*:*:*:*:*:*:*",
+                        "referenceType": "cpe23Type",
+                    }
+                ],
+                "primaryPackagePurpose": "SOURCE",
+                "downloadLocation": "https://www.python.org/ftp/python/3.13.0/fake-artifact.txt",
+                "checksums": [
+                    {
+                        "algorithm": "SHA256",
+                        "checksumValue": "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                    }
+                ],
+            }
+        ],
+        "SPDXID": "SPDXRef-DOCUMENT",
+        "spdxVersion": "SPDX-2.3",
+        "name": "CPython SBOM",
+        "dataLicense": "CC0-1.0",
+        "documentNamespace": "https://www.python.org/ftp/python/3.13.0/fake-artifact.txt.spdx.json",
+        "creationInfo": {
+            "creators": [
+                "Person: Python Release Managers",
+            ],
+            "licenseListVersion": "3.22",
+        },
+    }
+
+
+@pytest.mark.parametrize(
+    ["cpython_version", "download_location"],
+    [
+        ("3.13.0", "https://www.python.org/ftp/python/3.13.0/fake-artifact.txt"),
+        ("3.11.0a1", "https://www.python.org/ftp/python/3.11.0/fake-artifact.txt"),
+        ("3.12.0b2", "https://www.python.org/ftp/python/3.12.0/fake-artifact.txt"),
+        ("3.13.0rc3", "https://www.python.org/ftp/python/3.13.0/fake-artifact.txt"),
+    ]
+)
+def test_create_cpython_sbom_pre_release_download_location(cpython_version, download_location):
+    sbom_data = {"packages": []}
+
+    artifact_path = str(pathlib.Path(__file__).parent / "fake-artifact.txt")
+    sbom.create_cpython_sbom(
+        sbom_data, cpython_version=cpython_version, artifact_path=artifact_path
+    )
+
+    assert sbom_data["packages"][0]["downloadLocation"] == download_location
