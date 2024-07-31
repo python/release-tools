70a9790bea46c537925fc81e26db25ce15f67a76
Łukasz Langa <lukasz@langa.pl>
2024-07-31 19:21:08 +0200

    Add a file that was .gitignore'd by shouldn't

---
+++ tests/magicdata/Lib/test/test_importlib/test_util.py
@@ -0,0 +1,7 @@
+# copied from cpython bd3d31f380cd451a4ab6da5fbfde463fed95b5b5
+
+
+class SomeClass:
+    def some_method(self) -> None:
+        EXPECTED_MAGIC_NUMBER = 3495
+        print(EXPECTED_MAGIC_NUMBER)
