33509212846cd8d08a565445528c9b58c855f4ca
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-01-20 12:27:33 +0200

    Remove EOL Python 3.9 from Sigstore ID list

--- add_to_pydotorg.py
+++ add_to_pydotorg.py
@@ -81,7 +81,6 @@ google_oidc_provider = "https://accounts.google.com"
 
 # Update this list when new release managers are added.
 release_to_sigstore_identity_and_oidc_issuer = {
-    "3.9": ("lukasz@langa.pl", github_oidc_provider),
     "3.10": ("pablogsal@python.org", google_oidc_provider),
     "3.11": ("pablogsal@python.org", google_oidc_provider),
     "3.12": ("thomas@python.org", google_oidc_provider),
